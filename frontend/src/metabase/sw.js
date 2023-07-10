d9dd95653bf3a6c9d9d0a4ecf47aee3bdbe56618
RobertoMaurizzi <roberto@catalpa.io>
2023-07-10 10:23:29 +0800

    Add workbox and placeholder service worker code

---
+++ frontend/src/metabase/sw.js
@@ -0,0 +1,42 @@
+import { registerRoute } from "workbox-routing";
+import {
+  StaleWhileRevalidate,
+  NetworkFirst,
+  NetworkOnly,
+} from "workbox-strategies";
+import { precacheAndRoute } from "workbox-precaching";
+
+precacheAndRoute(self.__WB_MANIFEST || []);
+
+// network first for HTML pages
+registerRoute(
+  ({ url }) => url.pathname === "/" || url.pathname === "/app/",
+  new NetworkFirst(),
+);
+
+// common XHR requests that can be slow
+// registerRoute(({url}) => url.pathname.endsWith("/jsi18n/"), new NetworkFirst());
+// registerRoute(({url}) => url.pathname.startsWith("/en/geo/data.geojson"), new StaleWhileRevalidate());
+
+// ensure quick response for static assets that use cache busting
+registerRoute(({ url, request, sameOrigin }) => {
+  return (
+    url.pathname.startsWith("/static/") &&
+    url.pathname.match(/.*\.[0-9a-f]{12}\..*/) &&
+    ["image", "script", "style", "font"].includes(request.destination) &&
+    sameOrigin
+  );
+}, new StaleWhileRevalidate());
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith("/api/"),
+  new NetworkOnly(),
+  "POST",
+);
+registerRoute(
+  ({ url }) => url.pathname.startsWith("/api/"),
+  new NetworkOnly(),
+  "DELETE",
+);
+
+addEventListener("install", () => self.skipWaiting());
