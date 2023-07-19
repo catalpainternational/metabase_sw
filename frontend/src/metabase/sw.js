12d7ed8117396a24ae339b20f76502d58c2f37f4
Roberto Maurizzi <roberto.maurizzi@gmail.com>
2023-07-19 13:56:28 +0800

    Add service worker (#1)
    Managed through workbox, stale while revalidate for static assets that use cache busting

---
+++ frontend/src/metabase/sw.js
@@ -0,0 +1,38 @@
+/* global importScripts, workbox */
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js",
+);
+
+// network first for HTML pages
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname === "/" || url.pathname === "/app/",
+  new workbox.strategies.NetworkFirst(),
+);
+
+// ensure quick response for static assets that use cache busting
+workbox.routing.registerRoute(({ url, request, sameOrigin }) => {
+  return (
+    url.pathname.startsWith("/app/dist/") &&
+    url.pathname.match(/.*\..*/) &&
+    url.search.match(/.*[0-9a-f]{20}.*/) &&
+    ["image", "script", "style", "font"].includes(request.destination) &&
+    sameOrigin
+  );
+}, new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname.startsWith("/api/"),
+  new workbox.strategies.NetworkOnly(),
+  "POST",
+);
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname.startsWith("/api/"),
+  new workbox.strategies.NetworkOnly(),
+  "DELETE",
+);
+
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkFirst());
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
